184c08db4845303776169f783ee4c97efaca2404
xj616 <xj616@ic.ac.uk>
2018-05-30 19:04:33 +0100

    initial structure of the app

---
+++ College PostersTests/College_PostersTests.swift
@@ -0,0 +1,36 @@
+//
+//  College_PostersTests.swift
+//  College PostersTests
+//
+//  Created by 姜曦 on 30/05/2018.
+//  Copyright © 2018 姜曦. All rights reserved.
+//
+
+import XCTest
+@testable import College_Posters
+
+class College_PostersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
