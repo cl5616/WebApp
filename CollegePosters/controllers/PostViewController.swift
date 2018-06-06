612a1f1ea235a249fa2a49c9a581f86740d7b17b
Pinchu <py416@ic.ac.uk>
2018-06-06 18:19:24 +0100

    text field now moves up when editing

--- CollegePosters/controllers/PostViewController.swift
+++ CollegePosters/controllers/PostViewController.swift
@@ -10,63 +10,19 @@ import UIKit
 import Photos
 import BSImagePicker
 
-class PostViewController: UIViewController, /*UIImagePickerControllerDelegate,*/ UINavigationControllerDelegate,
+class PostViewController: UIViewController, UINavigationControllerDelegate,
     UITextFieldDelegate {
-    
-    
-    
-    //@IBOutlet weak var imageCollection: UICollectionView!
-    //@IBOutlet weak var previewImage: UIImageView!
     
     @IBOutlet weak var selectButton: UIButton!
     @IBOutlet weak var nextButton: UIBarButtonItem!
     @IBOutlet weak var titleTextField: UITextField!
-    
     @IBOutlet weak var previewImage: PostImagesView!
-    //    var picker = UIImagePickerController()
 
     var selectedAssets = [PHAsset]()
     var selectedPhotos = [UIImage]()
     
-  /*  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
-            self.previewImage.image = image
-            selectButton.isHidden = true
-            self.nextButton.isEnabled = true
-        }
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-    }*/
     
     @IBAction func selectPressed(_ sender: Any) {
-        
-        /*picker.allowsEditing = true
-        
-        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-        
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
-            if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                self.picker.sourceType = .camera
-                self.present(self.picker, animated: true, completion: nil)
-            } else {
-                let alert = UIAlertController(title: "Invalid Photo Source", message: "No access to Camera.", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            }
-            
-        }))
-        
-        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in self.picker.sourceType = .photoLibrary
-            self.present(self.picker, animated: true, completion: nil)
-        }))
-        
-        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(actionSheet, animated: true, completion: nil)*/
         selectedAssets = []
         selectedPhotos = []
         let mulPicker = BSImagePickerViewController()
@@ -91,7 +47,7 @@ class PostViewController: UIViewController, /*UIImagePickerControllerDelegate,*/
     func convertAssetsToImages() -> Void {
         if selectedAssets.count != 0 {
             
-            var contentWidth: CGFloat = 0.0
+            //var contentWidth: CGFloat = 0.0
             for i in 0..<selectedAssets.count {
                 let manager = PHImageManager.default()
                 let option = PHImageRequestOptions()
@@ -103,20 +59,9 @@ class PostViewController: UIViewController, /*UIImagePickerControllerDelegate,*/
                 
                 let data = UIImageJPEGRepresentation(thumbnail, 0.7)
                 let newImage = UIImage(data: data!)
-                /*let newImageView = UIImageView(image: newImage)
-                let xCoord = view.frame.midX + view.frame.width * CGFloat(i)
-                scrollViewImages.addSubview(newImageView)
-                contentWidth += scrollViewImages.frame.width
-                newImageView.frame = CGRect(x: xCoord, y: scrollViewImages.frame.height / 2, width: scrollViewImages.frame.width, height: scrollViewImages.frame.height)*/
                 self.selectedPhotos.append(newImage! as UIImage)
             }
-            /*scrollViewImages.contentSize = CGSize(width: contentWidth, height: scrollViewImages.frame.height)
-            */
-            /*
-            self.previewImage.animationImages = self.selectedPhotos
-            self.previewImage.animationDuration = 3.0
-            self.previewImage.startAnimating()
- */
+            
             previewImage.selectedPhotos = self.selectedPhotos
             DispatchQueue.main.async {
                 self.previewImage.cv.reloadData()
@@ -137,19 +82,37 @@ class PostViewController: UIViewController, /*UIImagePickerControllerDelegate,*/
         self.view.endEditing(true)
     }
     
+    @objc func keyboardWillChange(notification: Notification) {
+        guard let keyBoardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        if notification.name == Notification.Name.UIKeyboardWillShow || notification.name == Notification.Name.UIKeyboardWillChangeFrame {
+            
+            view.frame.origin.y = -keyBoardRect.height + (self.navigationController?.navigationBar.frame.size.height)! + (UIApplication.shared.statusBarFrame.height)
+        } else {
+            view.frame.origin.y = UIApplication.shared.statusBarFrame.height
+        }
+        
+    }
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
         nextButton.isEnabled = false
-        //picker.delegate = self
         titleTextField.delegate = self
         self.navigationItem.rightBarButtonItem = nextButton
     }
-
-}
-
-class PostImageCell: UICollectionViewCell {
     
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+
 }
